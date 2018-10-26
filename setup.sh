d297074acb6a9831f8a9d3b91a699fcab1b4bc2d
Kalle Ilves <kalleilv@gmail.com>
2018-10-26 11:06:06 +0300

    Update setup script

--- setup.sh
+++ setup.sh
@@ -2,9 +2,20 @@
 
 CURRENT_PATH=$(pwd)
 
+echo $CURRENT_PATH
+
 if ! [ -x "$(command -v nvm)" ]; then
   echo 'node not installed, installing...'
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+cat <<EOT >> ~/.profile
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+EOT
+
+  source ~/.profile
+
   nvm install 11.0.0
   nvm alias default 11.0.0
 fi
