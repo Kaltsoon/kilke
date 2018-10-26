168ccc6ecf460cd77b6c1bcf48797e4617c5af4a
Kalle Ilves <kalleilv@gmail.com>
2018-10-26 11:27:34 +0300

    Update setup script

--- setup.sh
+++ setup.sh
@@ -8,12 +8,6 @@ if ! [ -x "$(command -v nvm)" ]; then
   echo 'node not installed, installing...'
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
-cat <<EOT >> ~/.bashrc
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-EOT
-
   source ~/.bashrc
 
   nvm install 11.0.0
