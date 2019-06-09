9493c0ddc0eab89fa9e369633056765d0d065984
Kalle Ilves <kalleilv@gmail.com>
2019-06-09 14:13:54 +0300

    Remove knexfile and .env files from repository

--- packages/api/knexfile.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    database: 'kilke',
-    user: 'postgres',
-    password: 'postgres',
-  },
-};
--- seeds/createPumps.js
+++
@@ -1,6 +0,0 @@
-exports.seed = function(knex) {
-  return knex('pumps').insert({
-    id: 'pump1',
-    display_name: 'Pump 1',
-  });
-};
